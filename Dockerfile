0a5c76449f584fedfae1eebe62ed607d49d88a20
th0rtz <me@th0rtz.fr>
2018-06-01 09:02:56 +0200

    FIX: log

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ RUN apt-get install -y postfix
 
 RUN apt-get install -y syslog-ng logrotate
 
+RUN sed -i -E 's/^(\s*)system\(\);/\1unix-stream("\/dev\/log");/' /etc/syslog-ng/syslog-ng.conf
+RUN sed -i 's/^#\(SYSLOGNG_OPTS="--no-caps"\)/\1/g' /etc/default/syslog-ng
+
 ADD /app/mynetwork /app/mynetwork
 ADD docker-entrypoint.sh docker-entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,39 +4,38 @@ set -euo pipefail
 
 envs=(
 	POSTFIX_HOSTNAME
+	POSTFIX_MYNETWORK
 	POSTFIX_PORTS
 	POSTFIX_DEBUG
 )
 
 VAR_CFGMAIN="/etc/postfix/main.cf"
-VAR_MYNETWORK="\/app\/mynetwork"
 
 if [ $1 == "setup" ]; then
-
+	echo "- Setup"
         # Configure mynetwork
 
-        echo "-- Configuring mynetwork
-	if [ -n $VAR_MYNETWORK ]; then
-		for i in ${VAR_MYNETWORK[@}];do
-			echo $i >> "/app/mynetwork"
-		done
-		VAR_MYNETWORK="\/app\/mynetwork"
-	else
-		VAR_MYNETWORK="\/app\/mynetwork"
-	fi
+        echo "-- Configuring mynetwork"
+	for i in ${POSTFIX_MYNETWORK[@]};do
+		echo "--- Adding network : $i"
+		echo $i >> "/app/mynetwork"
+	done
+	VAR_MYNETWORK="\/app\/mynetwork"
+	echo "-- End Mynetwork"
 
 	#Set mailname conf
 	echo "-- Setting mailname file"
 	echo $POSTFIX_HOSTNAME > /etc/postfix/mailname
+	echo "--- Mailname : $POSTFIX_HOSTNAME"
 
 	#Setting up configuration
 
 	echo "-- Modifying main.cf"
-	sed -i "s/\$myhostname ESMTP \$mail_name (Ubuntu)/$POSTFIX_HOSTNAME ESMTP/g" $VAR_CFGMAIN
+	sed -i "s/\$myhostname ESMTP \$mail_name  \(Ubuntu\)/$POSTFIX_HOSTNAME ESMTP/g" $VAR_CFGMAIN
 	sed -i "s/smtpd_use_tls=yes/smtpd_use_tls=no/g" $VAR_CFGMAIN
 	sed -i "s/myhostname =.*/myhostname = $POSTFIX_HOSTNAME/g" $VAR_CFGMAIN
 	sed -i "s/mydestination =/\#mydestination =/g" $VAR_CFGMAIN
-	sed -i "s/mynetworks =/mynetworks = $VAR_MYNETWORK/g" $VAR_CFGMAIN
+	sed -i "s/mynetworks =.*/mynetworks = $VAR_MYNETWORK/g" $VAR_CFGMAIN
 	
 	## Add more conf
 
@@ -44,12 +43,11 @@ if [ $1 == "setup" ]; then
 	echo "notify_classes=bounce" >> $VAR_CFGMAIN
 	echo "always_add_missing_headers = yes" >> $VAR_CFGMAIN
 	echo "local_header_rewrite_clients = static:all" >> $VAR_CFGMAIN
-	
+	echo "-- End Configuration"
 fi
 
-if [ $1 == "reload" ]; then
-	service postfix reload
-fi
-
+echo "- Start"
+/etc/init.d/syslog-ng restart
+/etc/init.d/postfix restart
 
 sleep 20000000000000
