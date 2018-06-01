9645d706ffefd9f064ab73ee9491e573c09b1fdc
th0rtz <me@th0rtz.fr>
2018-06-01 09:02:16 +0200

    ADD: script

---
+++ start-with-file.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+
+myfile=`cat source.ip`
+
+docker run -itd \
+	-e POSTFIX_HOSTNAME=smtp.toto.fr \
+	-e POSTFIX_MYNETWORK="$myfile" \
+	-v /home/postfix/logs/mail.log:/var/log/mail.log
+	--mount source=docker-smtp-spool,target=/var/spool/postfix
+	-p 25:25
+	thortz/docker-postfix:dev setup
---
+++ update-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+mydate=`date +%Y%m%d%H%M`
+mydocker=`docker ps|grep docker-postfix|awk {'print $1'}`
+
+echo "[$mydate] - Starting update of postfix"
+echo "[$mydate] - pulling latest image of docker-postfix"
+docker pull thortz/docker-postfix:latest
+echo "[$mydate] - Killing running docker ID"
+docker kill $ $mydocker
+echo "[$mydate] - Removing docker ID"
+docker rm $mydocker
+echo "[$mydate] - Starting the new image"
+./start-with-file.sh
+echo "[$mydate] - End of upgrade
+echo ""
